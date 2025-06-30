989977a062c0cdb1d44530880354e6571ca80e1a
kavin-csquare <kavin.sugunapandian@contentsquare.com>
2025-06-30 10:42:40 +0100

    chore(sr): cleanup imports

--- app/src/main/java/com/example/androidsampleapp/SimpleActivity.kt
+++ app/src/main/java/com/example/androidsampleapp/SimpleActivity.kt
@@ -4,7 +4,6 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
